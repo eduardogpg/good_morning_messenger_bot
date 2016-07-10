e8c5c57981a8482ec27bcca32f5a246019e83a52
Eduardo Ismael <eduardo78d@gmail.com>
2016-07-10 13:32:27 -0500

    Echo

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ var bodyParser = require('body-parser');
 var request = require('request');
 var app = express();
 
+PAGE_ACCESS_TOKEN = 'EAAMf8hvIsAYBAFRutDrf88S6DlkfMopnH4pc45iZAl5pHvFasxasWn0zHIKFT4IGjTBFPBXZBv2Ro9bMlCl2Dc9s8XXiZCh3qtBmHEsZAlypg3HkVTljcP2fMGmtSFquKk4nznnPvhZCGSzPe2xIZCTEZAyBXui9p5DoN0NaddTNbysqRgJHjYq'
+CUSTOME_PORT = 3000
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.listen((process.env.PORT || 3000));
-
+app.listen((process.env.PORT || CUSTOME_PORT));
 
 app.get('/', function (req, res) {
-    res.send('Hello World');
+    res.send('Hola Mundo');
 });
 
 app.get('/webhook', function (req, res) {
@@ -20,5 +22,56 @@ app.get('/webhook', function (req, res) {
     }
 });
 
+app.post('/webhook', function (req, res) {
+  var data = req.body;
+ 
+  if (data.object == 'page') {
+    data.entry.forEach(function(pageEntry) {
 
-console.log("El servidor se encuentra en el puerto 3000")
+      pageEntry.messaging.forEach(function(messagingEvent) {
+      	
+      	if (messagingEvent.message) {
+          receivedMessage(messagingEvent);
+        }
+
+      });
+    });
+    res.sendStatus(200);
+  }
+});
+
+function receivedMessage(event) {
+  var senderID = event.sender.id;
+  var message = event.message;
+  var messageText = "I say : " + message.text;
+  sendTextMessage(senderID, messageText);
+}
+
+function sendTextMessage(recipientId, messageText) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: messageText
+    }
+  };
+
+  callSendAPI(messageData);
+}
+
+function callSendAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: PAGE_ACCESS_TOKEN },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+  	if (error){
+  		console.error("Error al enviar el mensaje.");
+  	}else{
+  		console.log("El mensaje fue enviado correctamente.");
+  	}
+  });  
+}
