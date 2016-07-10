f77f8214031358f16f1374a959987d5f6e41a680
Eduardo Ismael <eduardo78d@gmail.com>
2016-07-10 13:06:03 -0500

    Hello World Express

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Hola Mundo")
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.listen((process.env.PORT || 3000));
+
+
+app.get('/', function (req, res) {
+    res.send('Hello World');
+});
+
+app.get('/webhook', function (req, res) {
+    if (req.query['hub.verify_token'] === 'test_bot_say_hello') {
+        res.send(req.query['hub.challenge']);
+    } else {
+        res.send('Invalid verify token');
+    }
+});
+
+
+console.log("El servidor se encuentra en el puerto 3000")
