89dd3f37d2f7a091aa4f550eeb02490f38454bfe
Eduardo Ismael <eduardo78d@gmail.com>
2016-07-14 12:53:36 -0500

    refactor

--- index.js
+++ index.js
@@ -1,97 +1,90 @@
+/*===== Imports ===== */
 var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
+
+/*===== Const ===== */
+const PAGE_ACCESS_TOKEN = 'EAAQMMevRNgQBAIS3Y7RavTequW75QyBRm8ELXLxEcNQl39UPOh2fBhO7c25oeTIrYIHsRbvZCKB6MMlzf76WdNmYra7dGridz3l2Py0zXErPHvCBon1F0eHtoFZCOWOq0sbaoZCUigNq2KGlTDMWJut7cpRBDuO2hcuXZAYge6G0jiAm2DBZB'
+const IMAGE_DOGS = ['IZCWuqy', 'bdh4Qpn', '2cGhWub', 'tjvD7lA', 'A3DbC8r', 'jl4Oje0', 'nwClYm0', '93hRNQG', 'BdJtY']
+const IMAGE_CATS = ['SOFXhd6', 'C2IjdGW', 'h5mowK5' , '9uRmqxv', 'R63cYVM', 'EuQ4fPQ' , 'JniS4MF', 'DxhmtB5']
+
+/*===== Instances ===== */
 var app = express();
-
-PAGE_ACCESS_TOKEN = 'EAAENT4pPJkkBAEWi0nBn7uZBYdGFcKDt3g9HUwufWxBjp8Nw2mX7T4j5CBxz8sR4JAYxs1bYqLO6rZC8GY0Mw73jW0STtanXe9DfjmeF9OuE3mBflJ2ugH9Cb575xDHB4m49OmNGVgsaQtViEEXe3rZCJc6bVLClJ0yMNY8f33KaNBc5jeE'
-CUSTOME_PORT = 3000
-REPEAT_WORLD = "repite "
-WEATHER = "clima"
-TEMPERATURE = "temperatura"
-WORKSHOP = "taller"
-
-CAT = "gato"
-DOG = "perro"
-HELP = "ayuda"
-
-MESSAGE_HELP = "Puede pedir : el clima, imagenes de gatos de perro e información del taller"
-IMAGE_DOGS = ['IZCWuqy.jpg', 'bdh4Qpn.jpg', '2cGhWub', 'tjvD7lA', 'A3DbC8r', 'jl4Oje0', 'nwClYm0', '93hRNQG', 'BdJtY']
-IMAGE_CATS = ['SOFXhd6.jpg', 'C2IjdGW.jpg', 'h5mowK5.jpg' , '9uRmqxv.jpg', 'R63cYVM.jpg', 'EuQ4fPQ.jpg' , 'JniS4MF', 'DxhmtB5']
-URL_IMAGE = 'http://i.imgur.com/'
-
-
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-var server = app.listen( CUSTOME_PORT, function(){
-  var port = server.address().port
-  console.log("El servidor se encuentra a la escucha en el puerto " + port)
+var server = app.listen( 3000, function(){
+  console.log("El servidor se encuentra a la escucha en el puerto 3000" )
 });
 
-
 app.get('/', function (req, res) {
-  res.send('Hola Mundo');
+  res.send('Hello World');
 });
 
 app.get('/webhook', function (req, res) {
   if (req.query['hub.verify_token'] === 'test_bot_say_hello') {
-      res.send(req.query['hub.challenge']);
+    res.send(req.query['hub.challenge']);
   } else {
-      res.send('Invalid verify token');
+    res.send('Invalid verify token');
   }
 });
 
 app.post('/webhook', function (req, res) {
   var data = req.body;
   if (data.object == 'page') {
+      
     data.entry.forEach(function(pageEntry) {
       pageEntry.messaging.forEach(function(messagingEvent) {
         
         if (messagingEvent.message) {
           receivedMessage(messagingEvent);
         }
+
       });
     });
     res.sendStatus(200);
   }
+
 });
 
+/*======= Facebook Function =======*/
 function receivedMessage(event) {
   var senderID = event.sender.id;
   var message = event.message;
   var messageText = message.text;
 
   if (messageText) {
-    if (isContainsWorld(messageText, REPEAT_WORLD)){
+    if (isContainsWorld(messageText, 'repite') || isContainsWorld(messageText, 'di') ){
       
       message = repeatWorld(messageText);
       sendTextMessage(senderID, message);
 
-    }else if (isContainsWorld(messageText, WEATHER) || isContainsWorld(messageText, TEMPERATURE) ){
+    }else if (isContainsWorld(messageText, 'clima') || isContainsWorld(messageText, 'temperatura') ){
       
       GetWeather( function(message){
          sendTextMessage(senderID, message);
       });
 
-    }else if (isContainsWorld(messageText, CAT)  ){
+    }else if (isContainsWorld(messageText, 'gato')  ){
 
-      sendImageCat(senderID);
+      url_image = getUrlImage(IMAGE_CATS);
+      sendImageMessage(senderID, url_image);
 
-    }else if (isContainsWorld(messageText, DOG)  ){
+    }else if (isContainsWorld(messageText, 'perro')  ){
 
-      sendImageDog(senderID);
+      url_image = getUrlImage(IMAGE_DOGS)
+      sendImageMessage(senderID, url_image);
 
-    }else if (isContainsWorld(messageText, HELP)  ){
-  
-      sendTextMessage(senderID, MESSAGE_HELP);
+    }else if (isContainsWorld(messageText, 'ayuda')  ){
+      
+      sendMessageHelp(senderID);
 
-    }else if (isContainsWorld(messageText, WORKSHOP) ){
+    }else if (isContainsWorld(messageText, 'taller') ){
 
       var message = "El taller será el día viernes a las 5 de la tarde horario de México.";
       sendTextMessage(senderID, message);
 
     }else{
-      
       switch (messageText) {
       case 'generic':
         sendGenericMessage(senderID);
@@ -157,20 +150,9 @@ function sendTextMessage(recipientId, messageText) {
   callSendAPI(messageData);
 }
 
-
-function sendImageDog(recipientId){
- pos_image = Math.floor(Math.random() * IMAGE_DOGS.length );
- image = IMAGE_DOGS[pos_image]
- url_image = URL_IMAGE + image
- sendImageMessage(recipientId, url_image + ".jpg")
-}
-
-function sendImageCat(recipientId){
-  pos_image = Math.floor(Math.random() * IMAGE_CATS.length );
-  image = IMAGE_CATS[pos_image]
-  url_image = URL_IMAGE + image
-  console.log( url_image )
-  sendImageMessage(recipientId, url_image + ".jpg")
+function sendMessageHelp(recipientId){
+  var message = "Puede pedir : el clima, imagenes de gatos o de perros e información del taller";
+  sendTextMessage(recipientId, message);
 }
 
 function sendImageMessage(recipientId, url_image) {
@@ -215,14 +197,19 @@ function callSendAPI(messageData) {
     qs: { access_token: PAGE_ACCESS_TOKEN },
     method: 'POST',
     json: messageData
-
   }, function (error, response, body) {
     if (error){
-      console.error("Error al enviar el mensaje.\n");
+      console.error("Error al enviar el mensaje");
     }else{
-      console.log("El mensaje fue enviado correctamente.\n");
+      console.log("El mensaje fue enviado correctamente");
     }
   });  
+}
+
+/*======= Custome Function =======*/
+function getUrlImage( image_list ){
+  position = Math.floor(Math.random() * image_list.length );
+  return  'http://i.imgur.com/' + image_list[position] + ".jpg"
 }
 
 function isContainsWorld(messageText, sentence){
@@ -234,11 +221,9 @@ function repeatWorld(messageText){
 }
 
 function GetWeather( callback ){
-    //http://www.geonames.org/enablefreewebservice
    temperature = request('http://api.geonames.org/findNearByWeatherJSON?lat=16.750000&lng=-93.116669&username=eduardo_gpg', function (error, response, data) {
      response = JSON.parse(data);
      temperature = response.weatherObservation.temperature;
-
      final_message = "Me dicen que tenemos una temperatura de "+ temperature + " ºC"
      if (temperature > 30) {
         final_message+=  ", te recomiento que no salgas."
@@ -247,5 +232,5 @@ function GetWeather( callback ){
     }
      callback(final_message);
   });
-
 }
+
