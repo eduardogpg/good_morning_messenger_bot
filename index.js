079ff11062b4c41a49ac09463384706bf4ca7712
Eduardo Ismael <eduardo78d@gmail.com>
2016-07-14 21:08:12 -0500

    refactor total

--- index.js
+++ index.js
@@ -1,161 +1,113 @@
-/*===== Imports ===== */
 var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
 
-/*===== Const ===== */
-const PAGE_ACCESS_TOKEN = 'EAAQMMevRNgQBAIS3Y7RavTequW75QyBRm8ELXLxEcNQl39UPOh2fBhO7c25oeTIrYIHsRbvZCKB6MMlzf76WdNmYra7dGridz3l2Py0zXErPHvCBon1F0eHtoFZCOWOq0sbaoZCUigNq2KGlTDMWJut7cpRBDuO2hcuXZAYge6G0jiAm2DBZB'
-const IMAGE_DOGS = ['IZCWuqy', 'bdh4Qpn', '2cGhWub', 'tjvD7lA', 'A3DbC8r', 'jl4Oje0', 'nwClYm0', '93hRNQG', 'BdJtY']
-const IMAGE_CATS = ['SOFXhd6', 'C2IjdGW', 'h5mowK5' , '9uRmqxv', 'R63cYVM', 'EuQ4fPQ' , 'JniS4MF', 'DxhmtB5']
+const APP_TOKEN = 'EAASoUxc55QUBAJiI4T9vkfw58MmQGpRjCVFQW6HBD6dakk2srvivwTFvcGsKGiw0Kkmgz0HEbpPVZBZAEIydI3bNSbXVramCBa9Ed2rrosaeqRADsennk190yZBPoDfa6SkNHQbun0YhPZCcXGN0pHAPd2IEYoPLCZAVzM8isCepxquyOLt34'
 
-/*===== Instances ===== */
 var app = express();
-app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-var server = app.listen( 3000, function(){
-  console.log("El servidor se encuentra a la escucha en el puerto 3000" )
+
+var server = app.listen(3000, function(){
+  console.log("El servidor se encuentra en el puerto 3000");
 });
 
-app.get('/', function (req, res) {
-  res.send('Hello World');
+app.get('/', function(req, res){
+  res.send('Hola mundo');
 });
 
-app.get('/webhook', function (req, res) {
-  if (req.query['hub.verify_token'] === 'test_bot_say_hello') {
+
+app.get('/webhook', function(req, res){
+  if(req.query['hub.verify_token'] === 'test_token_say_hello' ){
     res.send(req.query['hub.challenge']);
-  } else {
-    res.send('Invalid verify token');
+  }else{
+    res.send('El token no es valido, lo siento');
   }
 });
 
-app.post('/webhook', function (req, res) {
+app.post('/webhook_dos', function(req, res){
   var data = req.body;
-  if (data.object == 'page') {
-      
-    data.entry.forEach(function(pageEntry) {
-      pageEntry.messaging.forEach(function(messagingEvent) {
-        
-        if (messagingEvent.message) {
-          receivedMessage(messagingEvent);
+  if(data.object == 'page'){
+    console.log(data);
+    res.sendStatus(200);
+  }
+});
+
+app.post('/webhook', function(req, res){
+
+  var data = req.body;
+  if(data.object == 'page'){
+    
+    data.entry.forEach(function(pageEntry){
+      pageEntry.messaging.forEach(function(messagingEvent){
+
+        if(messagingEvent.message){
+          receiveMessage(messagingEvent);
         }
 
       });
     });
+
     res.sendStatus(200);
   }
-
 });
 
-/*======= Facebook Function =======*/
-function receivedMessage(event) {
+function receiveMessage(event){
   var senderID = event.sender.id;
-  var message = event.message;
-  var messageText = message.text;
+  var messageText = event.message.text;
+  
+  if (messageText){
 
-  if (messageText) {
-    if (isContainsWorld(messageText, 'repite') || isContainsWorld(messageText, 'di') ){
-      
-      message = repeatWorld(messageText);
-      sendTextMessage(senderID, message);
-
-    }else if (isContainsWorld(messageText, 'clima') || isContainsWorld(messageText, 'temperatura') ){
-      
-      GetWeather( function(message){
-         sendTextMessage(senderID, message);
-      });
-
-    }else if (isContainsWorld(messageText, 'gato')  ){
-
-      url_image = getUrlImage(IMAGE_CATS);
-      sendImageMessage(senderID, url_image);
-
-    }else if (isContainsWorld(messageText, 'perro')  ){
-
-      url_image = getUrlImage(IMAGE_DOGS)
-      sendImageMessage(senderID, url_image);
-
-    }else if (isContainsWorld(messageText, 'ayuda')  ){
-      
-      sendMessageHelp(senderID);
-
-    }else if (isContainsWorld(messageText, 'taller') ){
-
-      var message = "El taller será el día viernes a las 5 de la tarde horario de México.";
-      sendTextMessage(senderID, message);
-
-    }else{
-      switch (messageText) {
-      case 'generic':
-        sendGenericMessage(senderID);
-        break;
-      default:
-        messageText = "Solo se repetir las cosas, escribe ayuda para más info.  " +messageText;
-        sendTextMessage(senderID, messageText);
-      }  
-    }
-  } 
-}
-
-function sendGenericMessage(recipientId) {
-  new_elements = []
-  new_elements.push( getGenericElementMessage() )
-
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {  
-      attachment: {
-        type: "template",
-        payload: {
-          template_type: "generic",
-          elements: new_elements,
-        }
-      }
-    }
-  };  
-  callSendAPI(messageData);
-}
-
-function getGenericElementMessage(){
-  new_buttons = []
-  new_buttons.push( getButtonMessage() )
-  return {
-    title: "Hello world",
-    subtitle: "Next-generation virtual reality",
-    item_url: "https://www.oculus.com/en-us/rift/",               
-    image_url: "http://messengerdemo.parseapp.com/img/rift.png",
-    buttons: new_buttons,
+    evaluateMessage(senderID, messageText);
+  
   }
 }
 
-function getButtonMessage(){
-  return {
-    type: "web_url",
-    url: "https://www.oculus.com/en-us/rift/",
-    title: "Open Web URL"
+function evaluateMessage(senderID, message){
+  var finalMessage = '';
+
+  if(isContains(message, 'taller')){
+
+    finalMessage = 'El taller esta en vivo';
+
+  }else if(isContains(message, 'gato')){
+
+    var url_image = getUrlImageCats();
+    sendMessageImage(senderID, url_image);
+
+  }else if(isContains(message, 'perro')){
+
+    var url_image = getUrlImageDogs();
+    sendMessageImage(senderID, url_image)
+  
+  }else if(isContains(message, 'clima')){
+
+    sendMessageWeather(senderID);
+
+  }else if(isContains(message, 'desarrollador')){
+
+    sendMessageHelp(senderID);
+
+  }else{
+
+    finalMessage = message;
   }
+  sendMessageText(senderID, finalMessage);
 }
 
-function sendTextMessage(recipientId, messageText) {
+function sendMessageText(recipientId,message){
   var messageData = {
     recipient: {
       id: recipientId
     },
     message: {
-      text: messageText
+      text: message
     }
   };
   callSendAPI(messageData);
 }
 
-function sendMessageHelp(recipientId){
-  var message = "Puede pedir : el clima, imagenes de gatos o de perros e información del taller";
-  sendTextMessage(recipientId, message);
-}
-
-function sendImageMessage(recipientId, url_image) {
+function sendMessageImage(recipientId, image_url){
   var messageData = {
     recipient: {
       id: recipientId
@@ -164,40 +116,21 @@ function sendImageMessage(recipientId, url_image) {
       attachment: {
         type: "image",
         payload: {
-          url: url_image
+          url: image_url, //"http://i.imgur.com/SOFXhd6.jpg" 
         }
       }
     }
   };
-
   callSendAPI(messageData);
 }
 
-function sendGifMessage(recipientId) {
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      attachment: {
-        type: "image",
-        payload: {
-          url: "http://messengerdemo.parseapp.com/img/instagram_logo.gif"
-        }
-      }
-    }
-  };
-
-  callSendAPI(messageData);
-}
-
-function callSendAPI(messageData) {
+function callSendAPI(messageData){
   request({
     uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: PAGE_ACCESS_TOKEN },
+    qs: { access_token: APP_TOKEN },
     method: 'POST',
     json: messageData
-  }, function (error, response, body) {
+  }, function (error, response, data) {
     if (error){
       console.error("Error al enviar el mensaje");
     }else{
@@ -206,31 +139,91 @@ function callSendAPI(messageData) {
   });  
 }
 
-/*======= Custome Function =======*/
-function getUrlImage( image_list ){
-  position = Math.floor(Math.random() * image_list.length );
-  return  'http://i.imgur.com/' + image_list[position] + ".jpg"
-}
+function sendMessageHelp(recipientId){
 
-function isContainsWorld(messageText, sentence){
-  return messageText.indexOf(sentence) > -1
-}
-
-function repeatWorld(messageText){
-  return messageText.replace(REPEAT_WORLD, "");
-}
-
-function GetWeather( callback ){
-   temperature = request('http://api.geonames.org/findNearByWeatherJSON?lat=16.750000&lng=-93.116669&username=eduardo_gpg', function (error, response, data) {
-     response = JSON.parse(data);
-     temperature = response.weatherObservation.temperature;
-     final_message = "Me dicen que tenemos una temperatura de "+ temperature + " ºC"
-     if (temperature > 30) {
-        final_message+=  ", te recomiento que no salgas."
-    }else{
-        final_message+=  ", un bonito día para disfrutar."
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [ templateElementHelp() ],
+        }
+      }
     }
-     callback(final_message);
+  };
+  callSendAPI(messageData);
+}
+
+function templateElementHelp(){
+  return {
+    title: "Eduardo Ismael",
+    subtitle: "Desarrollado de Software en Código facilito",
+    item_url: "https://www.facebook.com/Eduardo.Ismael.GP",               
+    image_url: "http://graph.facebook.com/100000141857371/picture?type=large",
+    buttons: [ buttonTwitter(), buttonFacebook() ],
+  }
+}
+
+function buttonTwitter(){
+  return {
+    type: "web_url",
+    url: "https://twitter.com/eduardo_gpg",
+    title: "Twitter"
+  }
+}
+function buttonFacebook(){
+  return {
+    type: "web_url",
+    url: "https://www.facebook.com/Eduardo.Ismael.GP",
+    title: "Facebook"
+  }
+}
+
+function sendMessageWeather(senderID){
+  getWeather(function(response){
+
+    message = validateTemperature(response);
+    sendMessageText(senderID, message);
+
   });
 }
 
+function getUrlImageDogs(){
+  var images = ['http://i.imgur.com/IZCWuqy.jpg', 'http://i.imgur.com/bdh4Qpn.jpg', 'http://i.imgur.com/2cGhWub.jpg', 'http://i.imgur.com/tjvD7lA.jpg', 'http://i.imgur.com/A3DbC8r.jpg', 'http://i.imgur.com/jl4Oje0.jpg', 'http://i.imgur.com/nwClYm0.jpg', 'http://i.imgur.com/93hRNQG.jpg', 'http://i.imgur.com/BdJtY.jpg']
+  return getRandomPosition(images);
+}
+
+function getUrlImageCats(){
+  var images = ['http://i.imgur.com/IZCWuqy.jpg', 'http://i.imgur.com/bdh4Qpn.jpg', 'http://i.imgur.com/2cGhWub.jpg', 'http://i.imgur.com/tjvD7lA.jpg', 'http://i.imgur.com/A3DbC8r.jpg', 'http://i.imgur.com/jl4Oje0.jpg', 'http://i.imgur.com/nwClYm0.jpg', 'http://i.imgur.com/93hRNQG.jpg', 'http://i.imgur.com/BdJtY.jpg']
+  return getRandomPosition(images);
+}
+
+function getRandomPosition(list){
+  var position = Math.floor(Math.random() * list.length );
+  return list[position];
+}
+
+function validateTemperature(temperature){
+  if (temperature > 30)
+    return 'El clima es ' + temperature + 'Cº te recomiendo que no salgas hay mucho calor';
+  return 'El clima es ' + temperature + 'Cº deberias salir a caminar';
+}
+
+function isContains(sentence, word){
+  return sentence.indexOf(word) > -1;
+}
+
+function getWeather( callback ){
+  request('http://api.geonames.org/findNearByWeatherJSON?lat=16.750000&lng=-93.116669&username=eduardo_gpg', 
+    function (error, response, data) {
+      if(!error){
+        response = JSON.parse(data);
+        temperature = response.weatherObservation.temperature;
+        callback(temperature);
+      }
+    });
+}
